33ec86eb96083c155fd160bfa30c6cd767a23a2e
Nathan Merrill <nathan.t.merrill@gmail.com>
2016-08-30 18:31:05 -0600

    Small fixes to powershell script

--- push.ps1
+++ push.ps1
@@ -4,12 +4,14 @@ git commit -m "Updates"
 git push origin HEAD:master
 cd ../
 git checkout master
-cp build/libs/StockExchange.jar StockExchange.jar
-java -jar StockExchange.jar download compile
+java -jar build/libs/StockExchange.jar   download compile
+rm StockExchange.jar
 git add .
 git commit -m "Updates"
 git push origin master
 git checkout archives
+git merge master
+cp build/libs/StockExchange.jar StockExchange.jar
 7z StockExchange.zip StockExchange.jar -aou
 7z StockExchange.zip submissions -aou
 7z StockExchangePlayers.zip submissions -aou
