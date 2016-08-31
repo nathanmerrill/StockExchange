53fe9c0323c0df426252b3290c10dde997e2d970
Nathan Merrill <nathan.t.merrill@gmail.com>
2016-08-30 19:14:53 -0600

    Fix to powershell script

--- push.ps1
+++ push.ps1
@@ -12,9 +12,9 @@ git push origin master
 git checkout archives
 git merge master
 cp build/libs/StockExchange.jar StockExchange.jar
-7z StockExchange.zip StockExchange.jar -aou
-7z StockExchange.zip submissions -aou
-7z StockExchangePlayers.zip submissions -aou
+7z a StockExchange.zip StockExchange.jar -aou
+7z a StockExchange.zip submissions -aou
+7z a StockExchangePlayers.zip submissions -aou
 git add .
 git commit -m "Updates"
 git push origin archives
